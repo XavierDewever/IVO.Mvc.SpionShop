e6eb584fffac20c756c171998c46fa7e73f8ceaa
Xavier Dewever <xavier.dewever@telenet.be>
2018-03-08 07:53:17 +0100

    Class Categorie

---
+++ IVO.Mvc.SpionShop.Web/Controllers/WinkelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IVO.Mvc.SpionShop.Web.Controllers
+{
+    public class WinkelController : Controller
+    {
+        // GET: Winkel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Winkel/Producten
+        public ActionResult Producten(string categorie)
+        {
+            return View();
+        }
+
+        // GET: /Winkel/Details
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
